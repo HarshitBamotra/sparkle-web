b1e15889ea9241a0a4e3d428ce4a786c37c8211b
Aniket Bamotra <aniketbamotra@gmail.com>
2022-01-31 23:16:45 +0530

    added www.thesparklee.com to allowed hosty

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sparkle2-env.eba-sdmjba65.us-west-2.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['sparkle2-env.eba-sdmjba65.us-west-2.elasticbeanstalk.com', '127.0.0.1', 'www.thesparklee.com']
 
 
 # Application definition
